628da05b8e267423d257ec53bd408112c03ddfcb
Opemipo Adeoyo <108328249+TdepbwT@users.noreply.github.com>
2024-08-19 11:14:50 +0100

    Camera movement estimation

--- camera_movement_estimator/camera_movement_estimator.py
+++ camera_movement_estimator/camera_movement_estimator.py
@@ -82,7 +82,7 @@ class CameraMovementEstimator():
     def draw_camera_movement(self, frames, camera_movement_per_frame):
         output_frames = []
 
-        for frame_num, frame in enumerate(frames):
+        for frame, (x_movement, y_movement) in zip(frames, camera_movement_per_frame):
             frame = frame.copy()
 
             overlay = frame.copy()
@@ -90,7 +90,6 @@ class CameraMovementEstimator():
             alpha = 0.6
             cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
 
-            x_movement, y_movement = camera_movement_per_frame[frame_num]
             frame = cv2.putText(frame, f"Camera Movement X: {x_movement:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 3)
             frame = cv2.putText(frame, f"Camera Movement Y: {y_movement:.2f}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 3)
 
--- main.py
+++ main.py
@@ -17,13 +17,15 @@ def main():
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path="stubs/track_stubs.pkl")
+    #get obj positions
+    tracker.add_position_to_tracks(tracks)
 
     #camera movement estimation
     camera_movement_estimator = CameraMovementEstimator(video_frames[0])
     camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames,
                                                                               read_from_stub=True,
                                                                               stub_path="stubs/camera_movement_stub.pkl")
-
+    camera_movement_estimator.add_adjust_positions_to_tracks(tracks, camera_movement_per_frame)
     #interpolate ball positions
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
 
